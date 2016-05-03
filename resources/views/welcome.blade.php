629a36f595d91e76431c080f56bd1271927ff370
AjaypratapAcme <ajay@acmeconnect.com>
2016-05-03 13:18:26 +0530

    Welcome edite some more text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 hello Ajay More text</div>
+                <div class="title">Laravel 5 hello Ajay More text Some more Text</div>
             </div>
         </div>
     </body>
